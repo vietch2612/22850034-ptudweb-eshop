efd61e1770332c5970d47125bd80bb6c31fa20fb
Viet Cao <viet.ch2612@gmail.com>
2023-05-21 00:05:12 +0700

    Fix validation when checkout

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -9,8 +9,8 @@ router.get('/checkout', controller.checkout);
 router.post('/placeorders',
     body('firstName').notEmpty().withMessage('First name is required!'),
     body('lastName').notEmpty().withMessage('Last name is required!'),
-    body('email').notEmpty().withMessage('Email is required!'),
-    body('mobile').notEmpty().withMessage('Mobile is required!').isEmail().withMessage('Invalid email address'),
+    body('email').notEmpty().withMessage('Email is required!').isEmail().withMessage('Invalid email address'),
+    body('mobile').notEmpty().withMessage('Mobile is required!'),
     body('address').notEmpty().withMessage('Address is required!'),
     (request, response, next) => {
         let errors = validationResult(request);
